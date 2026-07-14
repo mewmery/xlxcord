40890fb0e64a18af662b961a288a3a39f210fdfd
mewmery <oiinkgames@gmail.com>
2026-07-14 20:06:35 +1000

    Remove emoji from bot status message

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
- state: "🛡️ dont ping staff | dsc.gg/xalix",   // this is what people actually see
+ state: "dont ping staff | dsc.gg/xalix",   // this is what people actually see
         type: 4,               // Custom
       },
     ],
